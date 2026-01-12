c63f15a8ffbea21d63acde16c4951832973879df
Lorenzo Acerbo <lorenzo.acerbo@example.com>
2026-01-12 13:31:52 +0100

    fix: robustezza api vercel e gestione errori archivio

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
-import app from '../server/index.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const app = require('../server/index.js');
 
 // Vercel Serverless Function Handler
 export default function handler(req, res) {
--- server/index.js
+++ server/index.js
@@ -400,5 +400,3 @@ if (require.main === module) {
 }
 
 module.exports = app;
-
-module.exports = app;
